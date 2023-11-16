4d85060fbf1352823346ed7dd949d01a0eba49db
Ethan <ethancaff@gmail.com>
2023-11-16 12:55:07 +0000

    feat: :sparkles: Get all enchantments endpoint fully working
    Added service wrapper for enchantment; Get All Enchantments endpoint now produces JSON data from database; Enchantment table has a few more rows in it now;

--- EnchantmentRepo.js
+++ EnchantmentRepo.js
@@ -2,10 +2,10 @@ import { Database } from 'bun:sqlite';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { eq } from 'drizzle-orm';
 import enchantment from './models/enchantment';
+import db  from "./db/db";
 class EnchantmentRepo {
-    constructor(databasePath = "mydb.sqlite") {
-        const sqlite = new Database('mydb.sqlite');
-        this.db = drizzle(sqlite);
+    constructor() {
+
     }
   
     // Additional methods specific to the "enchantment" table
@@ -13,39 +13,62 @@ class EnchantmentRepo {
     // Get Enchantment by Name
     getByEnchantmentName(name) {
       // Use the parent class method with a parameterized table name
-      return this.db.select().from(enchantment).where(eq(enchantment.name, name)).values();
+      return db.select().from(enchantment).where(eq(enchantment.name, name)).values();
     }
 
     // Get all enchantments
     getAllEnchantments(){
-        return this.db.select().from(enchantment).values();
+        let enchantments =  db.select().from(enchantment).values();
+        let actualEnchants = [];
+        enchantments.forEach(function (enchantment) {
+            actualEnchants.push(new Enchantment(enchantment[0], enchantment[1], enchantment[2], enchantment[3], enchantment[4]));
+        })
+        return actualEnchants;
     }
 
     // Add Enchantment
     //INSERT INTO enchantment (type, description, name, max_level) VALUES ('melee','does fire damage', 'fire aspect', 5)
-    async addEnchantment(){
+    async addEnchantment(newType, newDesc, newName, newMax){
 
-        await this.db.insert(enchantment).values([{
-            type: "test type",
-            description: "test description",
-            name: "test name",
-            maxLevel: 6
-        },]);
+        await db.insert(enchantment).values([
+        {
+            type: newType,
+            description: newDesc,
+            name: newName,
+            maxLevel: newMax
+        }
+        ,]);
     }
 
-    // Update Enchantment
+    // Update Enchantment Type
     async updateEnchantmentType(newType, id){
-        await this.db.update(enchantment)
+        await db.update(enchantment)
         .set({type: newType})
         .where(eq(enchantment.id,id));
     }
 
+    // Update Enchantment Description
+    async updateEnchantmentType(newDesc, id){
+        await db.update(enchantment)
+        .set({description: newDesc})
+        .where(eq(enchantment.id,id));
+    }
+
     async deleteEnchantment(enchantmentName){
-        await this.db.delete(enchantment).where(eq(enchantment.name,enchantmentName));
+        await db.delete(enchantment).where(eq(enchantment.name,enchantmentName));
     }
   
     // Other methods specific to the "enchantment" table can be added here
   }
-  
+  class Enchantment {
+    constructor(id, type, description, name, maxLevel){
+        this.id = id;
+        this.type = type;
+        this.description = description,
+        this.name = name,
+        this.maxLevel = maxLevel
+    }
+
+}
   export default EnchantmentRepo;
   
--- index.js
+++ index.js
@@ -3,46 +3,23 @@ import Item from './models/item';
 import ItemHasEnchantment from './models/itemHasEnchantment';
 
 import EnchantmentRepo from './EnchantmentRepo'
-const repo = new EnchantmentRepo();
-
+import EnchantmentServiceWrapper from './EnchantmentServiceWrapper';
+// const repo = new EnchantmentRepo();
+const enchantSW = new EnchantmentServiceWrapper();
 
 // var insert = await repo.addEnchantment();
 // console.log(insert);
 
-var id = repo.getByEnchantmentName('test name')[0][0];
-console.log(id);
-// await repo.updateEnchantmentType('new type', 2);
-await repo.deleteEnchantment('test name');
-var results = repo.getAllEnchantments();
-console.log(results);
-// var result = repo.getByEnchantmentName('test name');
-// console.log(result);
+// var id = repo.getByEnchantmentName('fire aspect')[0][0];
+// console.log(id);
+// // await repo.updateEnchantmentType('new type', 2);
+// await repo.deleteEnchantment('test name');
+// var results = repo.getAllEnchantments();
+// console.log(results);
+// // var result = repo.getByEnchantmentName('test name');
+// // console.log(result);
 
 const responseBuilder = require('./util/responsebuilder.js');
-const dummyData = [
-  new Item('Diamond Sword', 'Weapon'),
-  new Item('Iron Pickaxe', 'Tool'),
-  new Item('Golden Apple', 'Consumable'),
-  new Item('Ender Pearl', 'Resource'),
-  new Item('Redstone Dust', 'Resource'),
-];
-const enchantmentData = [
-  new ItemHasEnchantment('Diamond Sword', 'Sharpness'),
-  new ItemHasEnchantment('Iron Pickaxe', 'Efficiency'),
-  new ItemHasEnchantment('Golden Apple', 'Protection'),
-  new ItemHasEnchantment('Ender Pearl', 'Unbreaking'),
-  new ItemHasEnchantment('Redstone Dust', 'Fortune'),
-];
-
-// const insertDataQuery = 'INSERT INTO enchantment (type, description, name, max_level) VALUES (?,?,?,?)';
-// const userData = ['melee','does fire damage', 'fire aspect', 5];
-// db.insertData(insertDataQuery, userData);
-// const readDataQuery = 'SELECT * FROM enchantment';
-// const data = db.getAllData(readDataQuery);
-// console.log(data);
-// const row = db.getByName('fire aspect','enchantment');
-// console.log(row.name);
-// db.close();
 
 const server = Bun.serve({
     port: 8080,
@@ -56,7 +33,9 @@ const server = Bun.serve({
       const {pathname} = new URL(url);
       if(pathname == "/enchantments" && method == "GET") {
         // Use map() to get an array of string representations
-        return responseBuilder.buildSimpleResponse('404 add the db',200);
+        let results = enchantSW.getAllEnchantments();
+        console.log(results);
+        return responseBuilder.buildSimpleResponse(JSON.stringify(results),200);
       }
 
       if(pathname == "/enchantments/name" && method == "GET") {
